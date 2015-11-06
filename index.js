2ef669e0de72d1c2c3e6a6e5087e2c57c669b1e2
Thai Pangsakulyanont <dtinth@spacet.me>
2015-11-07 01:24:11 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'standard',
+  rules: {
+    'indent': 0,
+    'no-multiple-empty-lines': 0,
+    'padded-blocks': 0,
+    'generator-star-spacing': [ 2, { before: false, after: true } ],
+    'spaced-comment': 0,
+  }
+}
