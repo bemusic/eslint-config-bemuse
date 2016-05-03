48c50999c078083b998b427517bf2fee7449edc6
Thai Pangsakulyanont <dtinth@spacet.me>
2016-05-03 08:26:22 +0700

    Update rules to cope with Standard v7

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 module.exports = {
-  extends: require.resolve('eslint-config-standard'),
+  extends: [
+    require.resolve('eslint-config-standard'),
+    require.resolve('eslint-config-standard-react'),
+  ],
   rules: {
+    'arrow-parens': 0,
     'comma-dangle': 0,
     'indent': 0,
+    'jsx-quotes': [ 2, 'prefer-double' ],
     'key-spacing': [ 2, { beforeColon: false, afterColon: true, mode: 'minimum' } ],
     'no-multiple-empty-lines': 0,
     'no-multi-spaces': 0,
+    'no-duplicate-imports': 0,
+    'no-return-assign': [ 2, 'except-parens' ],
     'padded-blocks': 0,
-    'generator-star-spacing': [ 2, { before: false, after: true } ],
     'spaced-comment': 0,
     'yoda': 0,
   }
